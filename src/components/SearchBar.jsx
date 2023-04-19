411a186554844c9f1f7e150c267975facf161273
Kevin Chacon <kevinchacon3211@gmail.com>
2023-04-18 20:25:16 -0600

    Use search list component + Add save button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,60 +1,80 @@
 import { useState } from 'react'
 import municipalities from '../data/municipalities.json'
+import SearchList from './SearchList'
 
-const removeAccents = (str) => {
-	return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-}
-
-export default function SearchBar({ muniSelected }) {
+export default function SearchBar({ municSelected, saveMunicipality }) {
 	const [municipality, setMunicipality] = useState('')
 	const [inputFocus, setInputFocus] = useState(false)
+	const [isSaved, setIsSaved] = useState(false)
+
+	const savedMunicipalities = JSON.parse(localStorage.getItem('savedMunicipalities')) ?? []
+
+	const checkIfSaved = (munic) => {
+		const saved = savedMunicipalities.find(({ id_mun: idMun }) => idMun === munic.id_mun)
+		return saved === undefined
+	}
 
 	const selectMunicipality = ({ mun }) => {
-		muniSelected(mun.name)
+		municSelected(mun)
+		setMunicipality(mun.name)
 		setInputFocus(false)
+		const savedCheck = checkIfSaved(mun)
+		setIsSaved(!savedCheck)
 	}
 
 	return (
 		<form className='flex justify-center w-full h-16 relative' onSubmit={(e) => e.preventDefault()}>
 			<div className='grid justify-center w-fit m-auto absolute'>
-				<div className='w-96 flex items-center'>
-					<input
-						className='w-full p-4 bg-blue-950 text-white border-2 border-gray-500 outline-none drop-shadow-black transition ease-in-out duration-500 focus:bg-gray-800'
-						type='text'
-						placeholder='Escribe el nombre de tu municipio'
-						onInput={(e) => setMunicipality(e.target.value)}
-						onFocus={() => setInputFocus(true)}
-						onBlur={() => setTimeout(() => setInputFocus(false), 500)}
-						name='municipio'
-						list='options'
-						title='Escribe el nombre de tu municipio'
+				<div className='flex'>
+					<div className='w-96 flex items-center'>
+						<input
+							className='w-full p-4 bg-blue-950 text-white border-2 border-gray-500 outline-none drop-shadow-black transition ease-in-out duration-500 focus:bg-gray-800'
+							type='text'
+							placeholder='Escribe el nombre de tu municipio'
+							value={municipality}
+							onInput={(e) => setMunicipality(e.target.value)}
+							onFocus={() => setInputFocus(true)}
+							onBlur={() => setTimeout(() => setInputFocus(false), 400)}
+							name='municipio'
+							list='options'
+							title='Escribe el nombre de tu municipio'
+						/>
+						<span className='relative right-9'>
+							<i className='fa-sharp fa-solid fa-magnifying-glass'></i>
+						</span>
+					</div>
+					<button
+						title='Guardar municipio'
+						className='w-[4rem] bg-blue-950 flex justify-center items-center text-lg border-2 border-gray-500 drop-shadow-black hover:bg-gray-800 transition ease-in-out duration-600'
+						onClick={() => {
+							saveMunicipality()
+							setIsSaved(true)
+						}}
+					>
+						{
+							isSaved
+							? <i className="fa-solid fa-bookmark"></i>
+							: <i className="fa-regular fa-bookmark"></i>
+						}
+					</button>
+				</div>
+				{
+					municipality.length > 0
+					? <SearchList
+						listType='searchResult'
+						inputFocus={inputFocus}
+						municipality={municipality}
+						municipalities={municipalities}
+						selectMunicipality={selectMunicipality}
 					/>
-					<span className='relative right-9'>
-						<i className='fa-sharp fa-solid fa-magnifying-glass'></i>
-					</span>
-				</div>
-				<div className='w-96 max-h-[192px] overflow-hidden block mt-1'>
-					{inputFocus && municipality.length > 0 && (
-						<ul className='text-left bg-slate-800'>
-							{municipalities.map((m) => {
-								const mun = removeAccents(m.name).toLowerCase()
-								const munInput = removeAccents(municipality).toLowerCase()
-								if (mun.includes(munInput)) {
-									return (
-										<li
-											key={m.id_mun}
-											onClick={() => selectMunicipality({ mun: m })}
-											className='h-15 p-3 hover:bg-slate-900 cursor-pointer'
-										>
-											{m.name}
-										</li>
-									)
-								}
-								return null
-							})}
-						</ul>
-					)}
-				</div>
+					: <SearchList
+						listType='savedMunicipalities'
+						inputFocus={inputFocus}
+						municipality={municipality}
+						savedMunicipality={savedMunicipalities}
+						selectMunicipality={selectMunicipality}
+					/>
+				}
 			</div>
 		</form>
 	)
