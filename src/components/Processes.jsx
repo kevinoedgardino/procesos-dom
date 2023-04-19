fa791a41bea870222b1f76ba7cce6532e3500733
Kevin Chacon <kevinchacon3211@gmail.com>
2023-04-18 20:26:30 -0600

    Use localstorage to save municipalities

--- src/components/Processes.jsx
+++ src/components/Processes.jsx
@@ -3,16 +3,30 @@ import SearchBar from './SearchBar'
 import FoundProcesses from './FoundProcesses'
 
 export default function Processes() {
-    const [municipalitySelected, setMunicipalitySelected] = useState('')
+    const [municipalitySelected, setMunicipalitySelected] = useState({})
 
     const getMunicSelected = (munic) => {
         setMunicipalitySelected(munic)
     }
 
+    const checkIfSaved = (munic, arrayData) => {
+		const saved = arrayData.find(({ id_mun: idMun }) => idMun === munic.id_mun)
+		return !(saved === undefined)
+	}
+
+    const saveMunicipality = () => {
+        const savedMunic = JSON.parse(window.localStorage.getItem('savedMunicipalities'))
+        const isSaved = checkIfSaved(municipalitySelected, savedMunic ?? [])
+
+        if (!isSaved) {
+            const newSavedMunic = savedMunic ? [...savedMunic, municipalitySelected] : [municipalitySelected]
+            window.localStorage.setItem('savedMunicipalities', JSON.stringify(newSavedMunic))
+        }
+    }
+
     return (
         <div>
-            <p>{municipalitySelected}</p>
-            <SearchBar muniSelected={getMunicSelected} />
+            <SearchBar municSelected={getMunicSelected} saveMunicipality={saveMunicipality} />
             <br />
             <FoundProcesses />
         </div>
