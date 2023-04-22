3c39a06e8b2eb328c32fc6dcb1f61ede62bd7704
Kevin Chacon <kevinchacon3211@gmail.com>
2023-04-21 19:26:40 -0600

    Change file name + add more colors for plot

--- src/components/ProcessCard.jsx
+++ src/components/ProcessCard.jsx
@@ -1,24 +1,15 @@
-export default function ProcessCard({ status, statusId, code, name, munic, date }) {
-    const statusTextColor = {
-        2: 'text-receiving',
-        3: 'text-evaluating',
-        7: 'text-preparing',
-        9: 'text-suspended',
-        10: 'text-noeffect',
-        12: 'text-denied',
-        13: 'text-desert',
-        16: 'text-hired'
-    }
+import statusTextColor from './statusColors'
 
+export default function ProcessCard({ status, statusId, code, name, munic, date }) {
 	return (
-        <div className="block bg-[#22344961] w-80 lg:w-96 p-2 self-center h-min rounded shadow-[0 0 0 1px #000] border-[#30363d] border-2 hover:brightness-110 active:brightness-125 cursor-pointer">
+        <div aria-label="Más detalles" role="button" className="block bg-[#22344961] w-80 lg:w-96 p-2 self-center h-min rounded shadow-[0 0 0 1px #000] border-[#30363d] border-2 hover:brightness-110 active:brightness-125 cursor-pointer">
             <header className="mb-1">
                 <div className="w-full flex justify-between mb-1 text-xs font-medium">
-                    <h3 className={statusTextColor[statusId]}>
+                    <h3 aria-label='Estado del proceso' className={statusTextColor[statusId]}>
                         <i className="fa-solid fa-circle-info me-1"></i>
                         <span>{status}</span>
                     </h3>
-                    <h3 className="text-slate-600">{code}</h3>
+                    <h3 aria-label='Código del proceso' className="text-slate-600">{code}</h3>
                 </div>
                 <h2 title={name} className="w-full text-left text-md mt-1 font-bold leading-5"><span className="line-clamp-3">{name}</span></h2>
             </header>
---
+++ src/components/statusColors.js
@@ -0,0 +1,18 @@
+export const statusTextColor = {
+    2: 'text-receiving',
+    3: 'text-evaluating',
+    7: 'text-preparing',
+    9: 'text-suspended',
+    10: 'text-noeffect',
+    12: 'text-denied',
+    13: 'text-desert',
+    16: 'text-hired'
+}
+
+export const plotStatusColor = {
+    3: 'text-red-500',
+    4: 'text-amber-500',
+    5: 'text-blue-400',
+    6: 'text-amber-500',
+    7: 'text-green-500'
+}
