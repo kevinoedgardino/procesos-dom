eca367c589107b865aa0c15593f94c4be51831d6
Kevin Chacon <kevinchacon3211@gmail.com>
2023-04-25 17:14:19 -0600

    Create utils folder + add global data dictionaries

---
+++ src/utils/globalData.js
@@ -0,0 +1,9 @@
+export const QUERY = {
+    ERROR: Symbol('Query Error'),
+    SUCCESS: Symbol('Query Success')
+}
+
+export const LIST_TYPE = {
+    SAVED_MUNICIPALITIES: Symbol('Saved Municipalities List'),
+    SEARCH_RESULT: Symbol('Saved Municipalities Result')
+}
