f7cdfc8992c79e1fd7240a751c447ac80d027ae8
Kevin Chacon <kevinchacon3211@gmail.com>
2023-04-25 16:11:21 -0600

    Add loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader () {
+    return (
+        <div className="h-10 overflow-hidden flex items-center">
+            <span className="loader"></span>
+        </div>
+    )
+}
