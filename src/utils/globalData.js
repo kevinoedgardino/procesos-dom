27e5a92f7e2ed37a024e131410926b8f500a00c4
Kevin Chacon <kevinchacon3211@gmail.com>
2023-04-25 18:17:07 -0600

    Document functions

--- src/utils/globalData.js
+++ src/utils/globalData.js
@@ -1,8 +1,14 @@
+/**
+ * Type or results of a query
+ */
 export const QUERY = {
     ERROR: Symbol('Query Error'),
     SUCCESS: Symbol('Query Success')
 }
 
+/**
+ * Type of search list for results
+ */
 export const LIST_TYPE = {
     SAVED_MUNICIPALITIES: Symbol('Saved Municipalities List'),
     SEARCH_RESULT: Symbol('Saved Municipalities Result')
