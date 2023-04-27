06fc548826fb738d30f3f05e5a57cb216d7cff57
Kevin Chacon <kevinchacon3211@gmail.com>
2023-04-27 11:03:10 -0600

    Add sitemap astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import react from '@astrojs/react'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: 'https://procesosdomsv.web.app',
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      lastmod: new Date()
+    })
+  ]
 })
