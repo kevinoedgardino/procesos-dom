6375d7c54627b3b0a651ff52a5318d98606a69bc
Kevin Chacon <kevinchacon3211@gmail.com>
2023-05-04 11:59:26 -0600

    Update coment

--- src/utils/checkSavedMunicipality.js
+++ src/utils/checkSavedMunicipality.js
@@ -1,7 +1,8 @@
 /**
- *
- * @param {Array} munic
- * @returns true when the municipality was found, false when the municipality wasn't found
+ * Check the localStorage item `savedMunicipalities` to find whether the municipality
+ * given is stored or not.
+ * @param {object} munic
+ * @returns {boolean} true when the municipality was found, false when the municipality wasn't found
 */
 export const checkIfSaved = (munic) => {
 	const savedMunicipalities = JSON.parse(localStorage.getItem('savedMunicipalities')) ?? []
